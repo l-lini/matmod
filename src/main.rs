7d83566d923dffc6c0d23370f80263d624acdd93
l-lini <119787571+l-lini@users.noreply.github.com>
2026-09-23 09:31:15 +0200

    triangle

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,19 @@
+use std::{borrow::Cow, sync::Arc};
+use tokio::runtime::Runtime;
+use wgpu::{
+    Color, CommandEncoderDescriptor, CurrentSurfaceTexture::*, Device, DeviceDescriptor,
+    FragmentState, Instance, InstanceDescriptor, LoadOp, MultisampleState, Operations,
+    PipelineCompilationOptions, PipelineLayoutDescriptor, PrimitiveState, PrimitiveTopology, Queue,
+    RenderPassColorAttachment, RenderPassDescriptor, RenderPipeline, RenderPipelineDescriptor,
+    RequestAdapterOptions, ShaderModule, ShaderModuleDescriptor, ShaderSource, StoreOp, Surface,
+    SurfaceConfiguration, TextureFormat, TextureViewDescriptor, VertexState,
+};
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
-    event_loop::{
-        ActiveEventLoop,
-        EventLoop,
-    },
-    window::{
-        Window,
-        WindowId,
-    },
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowId},
 };
-use wgpu::{
-    Color,
-    Queue,
-    Device,
-    LoadOp,
-    StoreOp,
-    Surface,
-    Instance,
-    Operations,
-    TextureFormat,
-    DeviceDescriptor,
-    InstanceDescriptor,
-    SurfaceConfiguration,
-    RenderPassDescriptor,
-    TextureViewDescriptor,
-    RequestAdapterOptions,
-    CommandEncoderDescriptor,
-    RenderPassColorAttachment,
-    CurrentSurfaceTexture::*,
-};
-use tokio::runtime::Runtime;
-use std::sync::Arc;
 
 enum Game<'s> {
     Uninitialized,
@@ -41,52 +23,89 @@ enum Game<'s> {
         window: Arc<Window>,
         surface: Surface<'s>,
         instance: Instance,
+        shader: ShaderModule,
+        pipeline: RenderPipeline,
         surface_configuration: SurfaceConfiguration,
-    }
+    },
 }
 
 impl<'s> ApplicationHandler for Game<'s> {
-    fn resumed(&mut self, event_loop: & ActiveEventLoop) {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         let window = Arc::new(
-            event_loop.create_window(
-                Window::default_attributes()
-            ).unwrap()
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
         );
 
         let display = event_loop.owned_display_handle();
 
-        let instance = Instance::new(
-            InstanceDescriptor::new_with_display_handle(
-                Box::new(display)
-            )
-        );
+        let instance = Instance::new(InstanceDescriptor::new_with_display_handle(Box::new(
+            display,
+        )));
 
         let surface = instance.create_surface(window.clone()).unwrap();
 
         let rt = Runtime::new().unwrap();
 
-        let adapter = rt.block_on(
-            instance.request_adapter(&RequestAdapterOptions {
+        let adapter = rt
+            .block_on(instance.request_adapter(&RequestAdapterOptions {
                 compatible_surface: Some(&surface),
                 ..Default::default()
-            })
-        ).unwrap();
+            }))
+            .unwrap();
 
-        let (device, queue) = rt.block_on(
-            adapter.request_device(
-                &DeviceDescriptor::default()
-            )
-        ).unwrap();
+        let (device, queue) = rt
+            .block_on(adapter.request_device(&DeviceDescriptor::default()))
+            .unwrap();
 
         let [width, height] = window.inner_size().into();
 
-        let surface_configuration = surface.get_default_config(
-            &adapter,
-            width,
-            height,
-        ).unwrap();
+        let surface_configuration = surface.get_default_config(&adapter, width, height).unwrap();
 
         surface.configure(&device, &surface_configuration);
+
+        let shader = device.create_shader_module(ShaderModuleDescriptor {
+            label: None,
+            source: ShaderSource::Wgsl(Cow::Borrowed(include_str!("shader.wgsl"))),
+        });
+
+        let pipeline_layout = device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: None,
+            bind_group_layouts: &vec![],
+            immediate_size: 0,
+        });
+
+        let pipeline = device.create_render_pipeline(&RenderPipelineDescriptor {
+            label: None,
+            layout: Some(&pipeline_layout),
+            vertex: VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &vec![],
+                compilation_options: PipelineCompilationOptions {
+                    constants: &vec![],
+                    zero_initialize_workgroup_memory: true,
+                },
+            },
+            fragment: Some(FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &vec![Some(TextureFormat::Bgra8UnormSrgb.into())],
+                compilation_options: PipelineCompilationOptions {
+                    constants: &vec![],
+                    zero_initialize_workgroup_memory: true,
+                },
+            }),
+            primitive: PrimitiveState {
+                topology: PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                ..Default::default()
+            },
+            multisample: MultisampleState::default(),
+            depth_stencil: None,
+            multiview_mask: None,
+            cache: None,
+        });
 
         *self = Game::Initialized {
             queue,
@@ -94,32 +113,51 @@ impl<'s> ApplicationHandler for Game<'s> {
             window,
             surface,
             instance,
+            shader,
+            pipeline,
             surface_configuration,
         };
     }
 
-    fn window_event(
-        &mut self,
-        event_loop: &ActiveEventLoop,
-        _id: WindowId,
-        event: WindowEvent
-    ) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => event_loop.exit(),
+            WindowEvent::Resized(size) => {
+                if let Game::Initialized {
+                    surface,
+                    device,
+                    surface_configuration,
+                    ..
+                } = self
+                {
+                    surface_configuration.width = size.width;
+                    surface_configuration.height = size.height;
+                    surface.configure(&device, &surface_configuration);
+                }
+            }
             WindowEvent::RedrawRequested => {
-                if let Game::Initialized { surface, queue, device, window, surface_configuration, instance, .. } = self {
+                if let Game::Initialized {
+                    surface,
+                    queue,
+                    device,
+                    window,
+                    pipeline,
+                    ..
+                } = self
+                {
                     match surface.get_current_texture() {
                         Success(texture) => {
-                            let mut command_encoder = device.create_command_encoder(&CommandEncoderDescriptor::default());
+                            let mut command_encoder =
+                                device.create_command_encoder(&CommandEncoderDescriptor::default());
                             {
                                 let view = &texture.texture.create_view(&TextureViewDescriptor {
                                     format: Some(TextureFormat::Bgra8UnormSrgb),
                                     ..Default::default()
                                 });
 
-                                let _ = command_encoder.begin_render_pass(&RenderPassDescriptor {
-                                    color_attachments: &[
-                                        Some(RenderPassColorAttachment {
+                                let mut render_pass =
+                                    command_encoder.begin_render_pass(&RenderPassDescriptor {
+                                        color_attachments: &vec![Some(RenderPassColorAttachment {
                                             view: &view,
                                             depth_slice: None,
                                             resolve_target: None,
@@ -127,11 +165,14 @@ impl<'s> ApplicationHandler for Game<'s> {
                                                 load: LoadOp::Clear(Color::WHITE),
                                                 store: StoreOp::Store,
                                             },
-                                        }),
-                                    ],
-                                    ..Default::default()
-                                });
+                                        })],
+                                        ..Default::default()
+                                    });
+
+                                render_pass.set_pipeline(&pipeline);
+                                render_pass.draw(0..3, 0..1);
                             }
+
                             let command_buffers = vec![command_encoder.finish()];
                             queue.submit(command_buffers);
 
@@ -139,7 +180,7 @@ impl<'s> ApplicationHandler for Game<'s> {
                             queue.present(texture);
 
                             window.request_redraw()
-                        },
+                        }
                         Suboptimal(_) => todo!("highly recomended to reconfigure"),
                         Timeout => todo!("skip and try again later"),
                         Occluded => todo!("skip frame and wait until not occluded"),
@@ -148,8 +189,10 @@ impl<'s> ApplicationHandler for Game<'s> {
                         Validation => todo!("attend to the validation error"),
                     }
                 }
-            },
-            _ => { dbg!(event); },
+            }
+            _ => {
+                dbg!(event);
+            }
         }
     }
 }
